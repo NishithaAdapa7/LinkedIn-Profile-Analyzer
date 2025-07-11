fa32c31480f2dddd1f9a64b60d4dac4aba83a39e
Nishitha <adapanishitha04@gmail.com>
2025-07-11 14:14:53 +0530

    Add render.yaml for Render deployment

--- venv/Scripts/activate.bat
+++ venv/Scripts/activate.bat
@@ -8,7 +8,7 @@ if defined _OLD_CODEPAGE (
     "%SystemRoot%\System32\chcp.com" 65001 > nul
 )
 
-set VIRTUAL_ENV=C:\Users\adapa\Resume-ATS-Score-Checker\venv
+set VIRTUAL_ENV=C:\Users\adapa\OneDrive\Desktop\ResAna\Resume-ATS-Score-Checker\venv
 
 if not defined PROMPT set PROMPT=$P$G
 
